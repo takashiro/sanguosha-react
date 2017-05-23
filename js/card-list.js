2a9ae76ca3b66e53f2e125a66de71bfd71dc6981
Takashiro <takashiro@qq.com>
2017-05-23 23:17:29 +0800

    Hand cards can be selected

--- js/card-list.js
+++ js/card-list.js
@@ -4,6 +4,17 @@ class CardList{
 	constructor(object){
 		this.object = object;
 		this.cards = [];
+
+		object.on('click', '.card', function(e){
+			var card = $(this);
+			if(card.hasClass('selected')){
+				card.removeClass('selected');
+				card.animate({'top' : '+=20px'}, 100, 'linear');
+			}else{
+				card.addClass('selected');
+				card.animate({'top' : '-=20px'}, 100, 'linear');
+			}
+		});
 	}
 
 	append(card){
@@ -12,15 +23,27 @@ class CardList{
 	}
 
 	update(){
-		var pos = this.object.offset();
+		var list = this.object;
+		var global_pos = list.offset();
+		var pos = {
+			'top' : 0,
+			'left' : 0
+		};
 		for(var i = 0; i < this.cards.length; i++){
 			var card = this.cards[i];
-			card.object.animate({
-				'left' : pos.left,
-				'top' : pos.top
+			var local_pos = {
+				'top' : pos.top,
+				'left' : pos.left
+			};
+			card.object.animate(global_pos, 500, 'swing', function(){
+				card.object.remove();
+				card.object.css(local_pos);
+				list.append(card.object);
 			});
 
-			pos.left += card.object.width() + 2;
+			var step = card.object.width() + 2;
+			global_pos.left += step;
+			pos.left += step;
 		}
 	}
 
