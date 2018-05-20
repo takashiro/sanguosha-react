0db071648026edca229cd9b11b6195b00b40e563
Takashiro <takashiro@qq.com>
2018-05-20 16:45:00 +0800

    Add Handcard Area

---
+++ src/page/Room/Dashboard/HandcardArea/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+class HandcardArea extends React.Component {
+
+	render() {
+		return <div className="handcard-area">
+		</div>;
+	}
+
+}
+
+export default HandcardArea;
