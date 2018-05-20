6eee8cb308e58426337c0b9e98307d55f6090539
Takashiro <takashiro@qq.com>
2018-05-20 18:32:45 +0800

    Add Main Area and Dashboard into Room

--- src/page/Room/index.js
+++ src/page/Room/index.js
@@ -3,10 +3,15 @@ import React from 'react';
 
 import './index.scss';
 
+import MainArea from './MainArea';
+import Dashboard from './Dashboard';
+
 class Room extends React.Component {
 
 	render() {
 		return <div className="room">
+			<MainArea />
+			<Dashboard />
 		</div>;
 	}
 
