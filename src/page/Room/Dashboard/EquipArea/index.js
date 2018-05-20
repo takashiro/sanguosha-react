e249f2b55d71307c952ce2ae336dbb72d27e6dda
Takashiro <takashiro@qq.com>
2018-05-20 16:41:26 +0800

    Add Equip Area

---
+++ src/page/Room/Dashboard/EquipArea/index.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+
+import './index.scss';
+
+class EquipArea extends React.Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			equips: [
+				null,
+				null,
+				null,
+				null,
+				null,
+			],
+		};
+	}
+
+	render() {
+		const equips = this.state.equips.map((value, i) => <li key={i}>
+		</li>);
+
+		return <ul className="equip-area">
+			{equips}
+		</ul>;
+	}
+
+}
+
+export default EquipArea;
