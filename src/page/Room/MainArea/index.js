568486abf99a9a2357688f2f81361167e4fda5c6
Takashiro <takashiro@qq.com>
2018-05-20 18:32:19 +0800

    Add Main Area

---
+++ src/page/Room/MainArea/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import './index.scss';
+
+class MainArea extends React.Component {
+
+	render() {
+		return <div className="main-area">
+		</div>;
+	}
+
+}
+
+export default MainArea;
