606724c0b4625022fa2e68bb3d358aa719b22d26
Takashiro <takashiro@qq.com>
2018-06-23 20:12:51 +0800

    Define commands as Enum

--- src/protocol.js
+++ src/protocol.js
@@ -1,7 +1,9 @@
 
-import command from './net/command';
+import Enum from './net/Enum';
+import net from './net/protocol';
 
-const cmd = command(
+const cmd = new Enum(
+	...net.enums,
 	'StartGame',
 	'ArrangeSeats',
 	'ChooseGeneral',
