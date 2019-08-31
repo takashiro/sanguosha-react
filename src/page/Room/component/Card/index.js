18f8ac0e061da217d1a1142c7b557f758a967e5b
Kazuichi Takashiro <takashiro@qq.com>
2019-08-31 14:59:15 +0800

    Add onClick listener to Card

--- src/page/Room/component/Card/index.js
+++ src/page/Room/component/Card/index.js
@@ -3,7 +3,28 @@ import React from 'react';
 
 import './index.scss';
 
+function onClick() {
+	this.setState(prev => {
+		const selected = !prev.selected;
+		const listener = selected ? this.props.onSelected : this.props.onUnselected;
+		if (listener) {
+			setTimeout(listener, 0, this.props.card);
+		}
+		return {selected};
+	});
+}
+
 class Card extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			selected: false,
+		};
+
+		this.onClick = props.selectable ? onClick.bind(this) : null;
+	}
 
 	render() {
 		const card = this.props.card;
@@ -14,8 +35,13 @@ class Card extends React.Component {
 		}
 
 		if (card.id()) {
+			const classNames = ['card', card.color()];
+			if (this.state.selected) {
+				classNames.push('selected');
+			}
+
 			style.backgroundImage = `url(style/card/${card.name()}.png)`;
-			return <div className={'card ' + card.color()} style={style}>
+			return <div className={classNames.join(' ')} style={style} onClick={this.onClick}>
 				<div className={'suit ' + card.suitString().toLowerCase()} />
 				<div className="number" style={{
 					backgroundImage: `url(style/card/number/${card.color()}/${card.number()}.png)`,
