e3226f9db2e50bf25699d233ddf867398c16b66f
Kazuichi Takashiro <takashiro@qq.com>
2019-10-04 16:03:50 +0800

    Remove react-trasition-group

--- src/page/Room/AnimationCanvas/index.js
+++ src/page/Room/AnimationCanvas/index.js
@@ -1,51 +1,16 @@
 
 import React from 'react';
 
-import AnimatedCard from './AnimatedCard';
-
 import './index.scss';
-
-function cleanCardMotions() {
-	this.setState(function (prev) {
-		const cardMotions = prev.cardMotions.filter(motion => motion.isValid());
-		return {cardMotions};
-	});
-}
-
-function onCardMove(motionGroup) {
-	motionGroup.on('destroyed', this.cleanCardMotions);
-
-	this.setState(function (prev) {
-		const motions = motionGroup.children();
-		const cardMotions = [...prev.cardMotions, ...motions];
-		return {cardMotions};
-	});
-}
 
 class AnimationCanvas extends React.Component {
 
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			cardMotions: []
-		};
-
-		const room = props.room;
-		room.on('cardmove', onCardMove.bind(this));
-
-		this.cleanCardMotions = cleanCardMotions.bind(this);
 	}
 
 	render() {
-		const cardMotions = this.state.cardMotions.filter(motion => motion.card());
 		return <div className="animation-canvas">
-			{cardMotions.map(motion => (
-				<AnimatedCard
-					key={`card-${motion.card().key()}`}
-					motion={motion}
-				/>
-			))}
 		</div>;
 	}
 
