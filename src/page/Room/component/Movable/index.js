da83d79d9fa03d8c8529b742be83fbdef7265b98
Kazuichi Takashiro <takashiro@qq.com>
2019-10-04 16:04:17 +0800

    Add class Movable

---
+++ src/page/Room/component/Movable/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import './index.scss';
+
+function onEnd() {
+	const { onEnd } = this.props;
+	if (onEnd) {
+		setTimeout(onEnd, 0);
+	}
+}
+
+class Movable extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		const { from } = props;
+
+		this.state = {
+			top: from.top,
+			left: from.left,
+			opacity: from.opacity || 0,
+			visible: false,
+		};
+
+		this.onEnd = onEnd.bind(this);
+	}
+
+	componentDidMount() {
+		const { to } = this.props;
+		if (!to) {
+			return;
+		}
+
+		this.timer = setTimeout(() => {
+			this.setState({
+				top: to.top,
+				left: to.left,
+				opacity: to.opacity,
+			});
+		}, 100);
+	}
+
+	componentWillUnmount() {
+		if (this.timer) {
+			clearTimeout(this.timer);
+			this.timer = null;
+		}
+	}
+
+	render() {
+		const style = {
+			top: this.state.top,
+			left: this.state.left,
+			opacity: this.state.opacity,
+		};
+
+		return <div className="movable" style={style} onTransitionEnd={this.onEnd}>
+			{this.props.children}
+		</div>;
+	}
+
+}
+
+export default Movable;
