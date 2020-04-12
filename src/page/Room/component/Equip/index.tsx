318decd472cf0d836e2e26288ae071f9e35ac2cd
Kazuichi Takashiro <takashiro@qq.com>
2020-04-12 16:11:28 +0800

    Add Equip

---
+++ src/page/Room/component/Equip/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import Card from '../../../../game/Card';
+
+import CardSuit from '../CardSuit';
+import CardNumber from '../CardNumber';
+
+import './index.scss';
+
+interface EquipProps {
+	card: Card;
+}
+
+interface EquipState {
+	selectable: boolean;
+	selected: boolean;
+}
+
+export default class Equip extends React.Component<EquipProps, EquipState> {
+	constructor(props: EquipProps) {
+		super(props);
+
+		this.state = {
+			selectable: false,
+			selected: false,
+		};
+	}
+
+	render(): JSX.Element {
+		const { card } = this.props;
+
+		const {
+			selectable,
+			selected,
+		} = this.state;
+
+		const classNames = ['equip'];
+		if (selectable) {
+			classNames.push('selectable');
+		}
+		if (selected) {
+			classNames.push('selected');
+		}
+
+		return (
+			<div className={classNames.join(' ')}>
+				<i className="icon sword" />
+				<span className="attack-range" />
+				<span className="name">{card.getName()}</span>
+				<CardSuit suit={card.getSuit()} />
+				<CardNumber color={card.getColor()} number={card.getNumber()} />
+			</div>
+		);
+	}
+}
