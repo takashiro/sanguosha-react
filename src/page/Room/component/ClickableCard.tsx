0f43fbf6f00c7fca1a0ae3e7708b67edc7a90ac1
Kazuichi Takashiro <takashiro@qq.com>
2020-05-04 13:05:57 +0800

    Add component ClickableCard

---
+++ src/page/Room/component/ClickableCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Card from '../../../game/Card';
+import CardItem from './Card';
+
+interface CardProps {
+	card: Card;
+	onClick?: (card: Card) => void;
+}
+
+export default class ClickableCard extends React.Component<CardProps, {}> {
+	handleClick = (): void => {
+		const { onClick } = this.props;
+		if (onClick) {
+			const { card } = this.props;
+			onClick(card);
+		}
+	}
+
+	handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>): void => {
+		if (event.key === 'Enter') {
+			this.handleClick();
+		}
+	}
+
+	render(): JSX.Element {
+		const { card } = this.props;
+
+		return (
+			<div className="clickable-card" role="button" tabIndex={0} onClick={this.handleClick} onKeyDown={this.handleKeyDown}>
+				<CardItem card={card} />
+			</div>
+		);
+	}
+}
