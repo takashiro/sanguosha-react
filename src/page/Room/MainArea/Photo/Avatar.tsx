7aebbfd27bd5073c0a624fec2a03b9d8ca6ec598
Kazuichi Takashiro <takashiro@qq.com>
2020-06-14 15:30:12 +0800

    Show general name of other players

--- src/page/Room/MainArea/Photo/Avatar.tsx
+++ src/page/Room/MainArea/Photo/Avatar.tsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { useIntl } from 'react-intl';
 import { GeneralProfile } from '@karuta/sanguosha-core';
 
 interface AvatarProps {
@@ -8,10 +9,11 @@ interface AvatarProps {
 }
 
 function Avatar(props: AvatarProps): JSX.Element {
+	const intl = useIntl();
 	const { general } = props;
 	const { position } = props;
 	const avatar = (general && general.name) || 'unknown';
-	const name = ''; // TODO: translate
+	const name = general && general.name ? intl.formatMessage({ id: `general.${general.name}` }) : '';
 
 	const className = `avatar ${position}`;
 	return (
